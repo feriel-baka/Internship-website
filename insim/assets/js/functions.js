842a663d622d22c493344a4ff0b5042e7d513625
feriel-baka <ferielbaka2019@gmail.com>
2021-07-28 22:57:27 +0100

    add two pages to the project

--- insim/assets/js/functions.js
+++ insim/assets/js/functions.js
@@ -488,7 +488,7 @@ Theme Version:	1.0.0
 						setCountDown();
 						setStylishScroll();
 						manageLeftSideMenu();
-						codeSecurity();
+						// codeSecurity();
 					},
 					
 					afterLoadThePage:function(){
